54d3cac775e2b6ead97c9a5d0f3ec9730ccdfc3a
Hossam Khaudariy <85705225+khaudariy@users.noreply.github.com>
2024-06-29 00:03:33 +0300

    Training on the list

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-// import Heeader from "./Heeader"
-// import POsts from './Posts';
-// import ButtonBorder from './ButtonBorder';
+import Heeader from "./Heeader"
+import POsts from './Posts';
+import ButtonBorder from './ButtonBorder';
 
 function App() {
-  const tasks = [
-    {id: 1 , title: "hossam"},
-    {id: 2 , title: "Hossam"},
-    {id: 3 , title: "h0ssam"},
-    {id: 4 , title: "ho$sam"},
-    {id: 5 , title: "hosam"},
-  ]
-  const mytasklist = tasks.map((tasks) => {
+  const posts = [
+    {id:1 , name: "Hossam" , article: "java"},
+    {id:2 , name: "Hossam2" , article: "java2"},
+    {id:3 , name: "Hossam3" , article: "java3"},
+    {id:4 , name: "Hossam4" , article: "java4"},
+    {id:5 , name: "Hossam5" , article: "java5"},
+  ];
+  const postlist= posts.map((post) => {
+    return <POsts key={post.id} name={post.name} article={post.article}  />
+
+  });
+  // const tasks = [
+  //   {id: 1 , title: "hossam"},
+  //   {id: 2 , title: "Hossam"},
+  //   {id: 3 , title: "h0ssam"},
+  //   {id: 4 , title: "ho$sam"},
+  //   {id: 5 , title: "hosam"},
+  // ]
+  // const mytasklist = tasks.map((tasks) => {
   
-      return (
-      <div key={tasks.id}>
-        <li>{tasks.title}</li>
-      </div>);
-      });
+  //     return (
+  //     <div key={tasks.id}>
+  //       <li>{tasks.title}</li>
+  //     </div>);
+  //     });
       return (
         <>
         
-         {mytasklist}
+        <Heeader/>
+    <ButtonBorder />
+    {postlist}
+
         
         </>
       )
     
-    {/* <Heeader/>
-    <ButtonBorder />
-    <POsts name = "Hossam1" article = "ARC1"/>
-    <POsts name = "Hossam2" article = "ARC2"/>
-    <POsts name = "Hossam3" article = "ARC3"/> */}
+    {/*  */}
     // <div>
 
     // </div>
