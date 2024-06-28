9758bc81e45ef7ec76d60cfa96b219fe0c25eb66
Hossam Khaudariy <85705225+khaudariy@users.noreply.github.com>
2024-06-28 22:51:11 +0300

    rendering data by the map fuction to make list

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-import Heeader from "./Heeader"
-import POsts from './Posts';
-import ButtonBorder from './ButtonBorder';
+// import Heeader from "./Heeader"
+// import POsts from './Posts';
+// import ButtonBorder from './ButtonBorder';
 
 function App() {
-  return (
-    <>
+  const tasks = [
+    {id: 1 , title: "hossam"},
+    {id: 2 , title: "Hossam"},
+    {id: 3 , title: "h0ssam"},
+    {id: 4 , title: "ho$sam"},
+    {id: 5 , title: "hosam"},
+  ]
+  const mytasklist = tasks.map((tasks) => {
+  
+      return (
+      <div key={tasks.id}>
+        <li>{tasks.title}</li>
+      </div>);
+      });
+      return (
+        <>
+        
+         {mytasklist}
+        
+        </>
+      )
     
-    <Heeader/>
+    {/* <Heeader/>
     <ButtonBorder />
     <POsts name = "Hossam1" article = "ARC1"/>
     <POsts name = "Hossam2" article = "ARC2"/>
-    <POsts name = "Hossam3" article = "ARC3"/>
+    <POsts name = "Hossam3" article = "ARC3"/> */}
+    // <div>
 
-  </>
-  );
-}
+    // </div>
+
+    }
 
 export default App;
